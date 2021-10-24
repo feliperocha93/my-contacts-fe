011940e40e6f29ebe03ff761dbaa38d87ed169bf
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2021-10-24 16:31:14 -0300

    Creating of the styled components Input and Select

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,9 +1,21 @@
 import PageHeader from '../../components/PageHeader';
 
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+
 export default function NewContact() {
   return (
-    <PageHeader
-      title="Novo contato"
-    />
+    <>
+      <PageHeader
+        title="Novo contato"
+      />
+
+      <Input placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+    </>
   );
 }
