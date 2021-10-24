e53ec72c306efabd0060364173a572d6542ce600
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2021-10-24 16:13:53 -0300

    Creating page header and installing propTypes

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,7 +1,9 @@
+import PageHeader from '../../components/PageHeader';
+
 export default function EditContact() {
   return (
-    <>
-      <h1>EditContact</h1>
-    </>
+    <PageHeader
+      title="Editar Felipe Rocha"
+    />
   );
 }
--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,7 +1,9 @@
+import PageHeader from '../../components/PageHeader';
+
 export default function NewContact() {
   return (
-    <>
-      <h1>NewContact</h1>
-    </>
+    <PageHeader
+      title="Novo contato"
+    />
   );
 }
