7b9766346397df2c4e989df2a5375039223c4796
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2021-10-25 09:54:45 -0300

    Creating Modal

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,12 +7,15 @@ import {
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
+import Modal from '../../components/Modal';
 
 const cards = [0, 1, 2];
 
 export default function Home() {
   return (
     <Container>
+      <Modal danger />
+
       <InputSearchContainer>
         <input type="text" placeholder="Pesquise pelo nome..." />
       </InputSearchContainer>
