7b0eb6d99b5dd2dcc3a482ac03040bb330d674c6
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2021-10-27 09:40:55 -0300

    Creating rules to disable submit button

--- src/components/ContatcForm/index.js
+++ src/components/ContatcForm/index.js
@@ -18,7 +18,14 @@ export default function ContatcForm({ buttonLabel }) {
   const [phone, setPhone] = useState('');
   const [category, setCategory] = useState('');
 
-  const { setError, removeError, getErrorMessageByFieldName } = useErrors();
+  const {
+    errors,
+    setError,
+    removeError,
+    getErrorMessageByFieldName,
+  } = useErrors();
+
+  const isFormValid = (name && errors.length === 0);
 
   function handleNameChange(event) {
     setName(event.target.value);
@@ -57,7 +64,7 @@ export default function ContatcForm({ buttonLabel }) {
       <FormGroup error={getErrorMessageByFieldName('name')}>
         <Input
           error={getErrorMessageByFieldName('name')}
-          placeholder="Nome"
+          placeholder="Nome *"
           value={name}
           onChange={handleNameChange}
         />
@@ -94,7 +101,7 @@ export default function ContatcForm({ buttonLabel }) {
       </FormGroup>
 
       <ButtonContainer>
-        <Button type="submit">
+        <Button type="submit" disabled={!isFormValid}>
           {buttonLabel}
         </Button>
       </ButtonContainer>
