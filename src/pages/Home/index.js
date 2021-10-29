a43749a2a73d980dddd9686258dafe330f617ce2
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2021-10-29 13:35:42 -0300

    Creating service layer and abstracting fetch contacts logic to it

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,7 +10,8 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 
 import Loader from '../../components/Loader';
-import delay from '../../utils/delay';
+
+import ContactsService from '../../services/ContactsService';
 
 export default function Home() {
   const [contacts, setContacts] = useState([]);
@@ -27,12 +28,9 @@ export default function Home() {
       try {
         setIsLoading(true);
 
-        const response = await fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`);
+        const contactsList = await ContactsService.listContacts(orderBy);
 
-        await delay(500);
-
-        const json = await response.json();
-        setContacts(json);
+        setContacts(contactsList);
       } catch (error) {
         console.log('error', error);
       } finally {
